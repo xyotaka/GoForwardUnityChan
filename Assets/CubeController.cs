da0ab37a6b0deb45e42eb515876f8d7d25177917
Bryan Thogerson <xyotaka@gmail.com>
2017-09-28 20:11:45 +0900

    GoForwardUnityChanの初コミット
    Cubeの効果音がついたものです。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    //AudioSource
+    private AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //衝突したオブジェクトのタグが「Cube」か「Ground」の場合、音を鳴らす
+        if(other.gameObject.tag == "Ground" || other.gameObject.tag == "Cube")
+        {
+            audio.Play();
+        }
+    }
+}
